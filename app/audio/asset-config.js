a442ed3a8984e653287a3e41f63cbcf26d02124b
sonnyg <sonny@evagabond.com>
2016-09-30 14:56:53 -0700

    may have gotten a little carried away with sounds. also alphabetized

--- app/audio/asset-config.js
+++ app/audio/asset-config.js
@@ -7,8 +7,19 @@ module.exports = {
   },
   alarmAssets: {
     none: "",
-    grandfather: "51826__ejfortin__grandfather-clock-ding-dong.wav",
+    baglama: "14421__jason01234__bit-of-baglama.wav",
+    banjo: "254943__tadworthington__banjonotes.wav",
     bluesGuitar: "203649__jack-master__blues-guitar-3.wav",
-    ding: "265012__sethlind__toaster-oven-ding.wav"
+    ding: "265012__sethlind__toaster-oven-ding.wav",
+    dmajor: "106748__frederic-font__gt1-dmajor.wav",
+    funk: "331365__orangefreesounds__funk-guitar-loop-114-bpm.wav",
+    grandfather: "51826__ejfortin__grandfather-clock-ding-dong.wav",
+    guitar: "54222__kaponja__guitar-be-bop3.wav",
+    harp: "2633__texasmusicforge__harp2.wav",
+    intro: "323974__scale75__semiq-intro-16.wav",
+    news: "181403__zagi2__thrill-announcement.wav",
+    paAlert: "274682__jppi-stu__sw-school-pa-alert.wav",
+    snakeCharmer: "260873__smuz__primitive-snake-charmer-melody.wav",
+    western: "124577__juskiddink__western-themetune2.wav"
   }
 }
